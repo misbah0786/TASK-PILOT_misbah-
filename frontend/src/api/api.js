9695862ea3cfa4de9b7b1f3bf8daf4d69456afcf
misbah0786 <misbahfirdose2020@gmail.com>
2026-05-30 20:45:32 +0530

    Fix API base URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:  "https://task-pilot-misbah.onrender.com",
+  baseURL: "https://task-pilot-misbah.onrender.com/api",
 });
 
 export default API;
